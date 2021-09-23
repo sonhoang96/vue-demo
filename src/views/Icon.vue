8dc50431b362ff8ab987458a90211c07bb897428
Son Tran Hoang (TWENDE) <os_sonth@etc.vn>
2021-09-23 17:11:08 +0700

    first commit

---
+++ src/views/Icon.vue
@@ -0,0 +1,30 @@
+<template #item.actions="{}">
+  <div>
+    <v-icon
+        small
+        class="mr-2"
+    >
+      mdi-pencil
+    </v-icon>
+    <v-icon
+        small
+        class="mr-2"
+    >
+      mdi-delete
+    </v-icon>
+    <span>{{ action }}</span>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Icon",
+    props: {
+      action: String,
+      data: String
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/ListData.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="about">
+    <h1>This is a list data</h1>
+    <v-app id="table">
+      <v-data-table
+          :headers="headers"
+          :items="listItem"
+          :items-per-page="5"
+          class="elevator"
+      >
+        <!-- header-->
+        <template #header.name="{header}"> {{ header.text.toUpperCase() }}</template>
+        <template #header.age="{header}"> {{ header.text.toUpperCase() }}</template>
+        <template #header.serial="{header}"> {{ header.text.toUpperCase() }}</template>
+        <template #header.actions="{header}"> {{ header.text.toUpperCase() }}</template>
+        <!--header-->
+
+        <Icon action="abc" data="xyz"/>
+
+      </v-data-table>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Icon from "./Icon"
+export default {
+  components: {
+    Icon
+  },
+  data: () => {
+    return {
+      headers: [
+        {text: "name", value: 'name'},
+        {text: "age", value: 'age'},
+        {text: "serial", value: 'serial'},
+        {text: "action", value: 'actions'}
+      ],
+      listItem: [
+        {
+          name: "ABC",
+          age: 23,
+          serial: "@#SSXXXXXXXXXXXX"
+        },
+        {
+          name: "ABC",
+          age: 23,
+          serial: "@#SSXXXXXXXXXXXX"
+        },
+        {
+          name: "ABC",
+          age: 23,
+          serial: "@#SSXXXXXXXXXXXX"
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
